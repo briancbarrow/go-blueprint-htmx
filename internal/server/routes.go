11b9298920ef7c7c5e7ac452637f9a053b812cd9
briancbarrow <briancbarrow@gmail.com>
2024-02-15 21:06:23 -0700

    remove hello world

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,8 +1,6 @@
 package server
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 
 	"go-blueprint-htmx/cmd/web"
@@ -16,26 +14,12 @@ func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", s.HelloWorldHandler)
 	fileServer := http.FileServer(http.FS(web.Files))
 	r.Handle("/css/*", fileServer)
 	r.Handle("/js/*", fileServer)
-	r.Get("/web", templ.Handler(web.Home()).ServeHTTP)
-	r.Post("/hello", web.HelloWebHandler)
+	r.Get("/", templ.Handler(web.Home()).ServeHTTP)
 
 	r.Post("/update_structure", web.UpdateStructureHandler)
 
 	return r
-}
-
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
-
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
-	}
-
-	_, _ = w.Write(jsonResp)
 }
